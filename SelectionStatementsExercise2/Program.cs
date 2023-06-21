bc5119892c743eac276ee4461e8d3fb2e0dc1d90
Eric Caro <c.aroericm@gmail.com>
2023-06-21 16:01:10 -0500

    Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Runtime.CompilerServices;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("What is your favorite school subject?");
+            
+            string subject = (Console.ReadLine());
+
+            Console.WriteLine("______________");
+
+            switch (subject.ToLower()) 
+            {
+                    case "math":
+                    Console.WriteLine("I hate Math");
+                    break;
+                    
+                    case "reading":
+                    Console.WriteLine("I don't like reading either");
+                    break;
+
+                    case "history":
+                    Console.WriteLine("History is cool");
+                    break;
+
+                    case "science":
+                    Console.WriteLine("Science is my favorite");
+                    break;
+
+                    case "Art":
+                    Console.WriteLine("Art is also fun");
+                    break;
+
+                    default:
+                    Console.WriteLine("I don't know or have that class");
+                    break;
+            }
+            }
     }
 }
